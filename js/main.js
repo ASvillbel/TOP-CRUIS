0d804edb2c71e196837d36749e17008450839052
ASvillbel <114774477+ASvillbel@users.noreply.github.com>
2023-05-08 00:03:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,11 @@ navBtn.onclick = () => {
         navBtnImg.src = "./img/open.svg";
     }
 }
+
+$('#form').submit(function() {
+    var faults = $('input').filter(function() {
+        // находим не заполненные обязательные элементы input
+        return $(this).data('required') && $(this).val() == "";
+    }).css("background-color", "red"); // выделяем это поле красным
+        if(faults.length) return false; // если одно из полей не заполнено, отменяем отправку
+    });
